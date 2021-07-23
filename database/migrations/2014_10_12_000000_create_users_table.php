44a5de920ece0580dbf0ae8bae603f17772c5bb5
Hans <hanscastroadmn@gmail.com>
2021-07-23 22:41:03 +0200

    Fix working with docker and mysql

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,14 +13,15 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+        Schema::create('users', function(Blueprint $table){
+            $table->increments('id');
+            $table->string('name',255);
+            $table->string('email',255);
+            $table->string('password',100)->nullable();
+            $table->string('role',100)->default('user');
             $table->rememberToken();
             $table->timestamps();
+            $table->engine = 'InnoDB';
         });
     }
 
