dc389d517ce93ed16779e14cb005c874d5de5e8a
Hans <hanscastroadmn@gmail.com>
2021-07-24 00:22:17 +0200

    Add user question log

---
+++ database/factories/UserQuestionLogFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\UserQuestionLog;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class UserQuestionLogFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = UserQuestionLog::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
