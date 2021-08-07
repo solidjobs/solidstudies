50ab010548faf16f9fc9503e6ec006fc82abd8d6
Hans <hanscastroadmn@gmail.com>
2021-08-07 14:19:05 +0200

    Quick start added to command.sh

--- command.sh
+++ command.sh
@@ -2,6 +2,15 @@
 
 case $1 in
 
+    setup)
+        php artisan migrate
+        php artisan db:seed
+        php artisan serve --host=0.0.0.0 --port=8001 > /dev/null 2>&1 &
+        echo "
+        setup and running
+        "
+        ;;
+
     serve)
         php artisan serve --host=0.0.0.0 --port=8001 > /dev/null 2>&1 &
         echo "
@@ -10,6 +19,7 @@ case $1 in
         ;;
     *)
         echo "
+        setup - migrate, seed and serve
         serve - php artisan serve --host=0.0.0.0 --port=8001 > /dev/null 2>&1 &
         "
         ;;
